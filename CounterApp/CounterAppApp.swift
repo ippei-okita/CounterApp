263935a90ba14458772f711951485b23343ec24d
ippei-okita <okpei1985@gmail.com>
2020-10-15 18:37:18 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 大北一平 on 2020/10/15.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
