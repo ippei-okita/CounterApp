bdb0d911c328bd8496d7c31a67bfbed9f09c1e9b
ippei-okita <okpei1985@gmail.com>
2020-10-17 16:48:16 +0900

    CounterApp用のレポジトリ作成

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+            Image("counter")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("\(number)")
+                .foregroundColor(.white)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            }
+            Button(action: {self.number += 1}) {
+                    Text("カウント")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .cornerRadius(10.0)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
